e64a9f84f98d9303dc1e3f0b295d9c815be8bbf0
kaitlyn.dodds <therealkadodds@kadodds.com>
2025-09-26 14:59:45 -0600

    initial commit

---
+++ src/main/java/kdodds/user_service/UserServiceApplication.java
@@ -0,0 +1,18 @@
+package kdodds.user_service;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kdodds/user_service/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package kdodds.user_service.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+
+}
